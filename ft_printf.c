b50df895c3b1171402ad3f52177335f53334f1fe
Sino- <SinisterSino@gmail.com>
2017-07-17 17:15:32 -0700

    testing

--- ft_printf.c
+++ ft_printf.c
@@ -1,20 +1,20 @@
-#include "libftprintf.h"
+#include /**/ "libftprintf.h" /**///"ft_printf.h"
 
 /*
- **	Allowed functions:
- **	◦ write
- **	◦ malloc
- **	◦ free
- **	◦ exit
- **	◦ The functions of man 3 stdarg
- **
- **	You have to manage the following conversions: sSpdDioOuUxXcC
- **	• You must manage %%
- **	• You must manage the flags #0-+ and space
- **	• You must manage the minimum field-width
- **	• You must manage the precision
- **	• You must manage the flags hh, h, l, ll, j, et z.
- */
+**	Allowed functions:
+**	◦ write
+**	◦ malloc
+**	◦ free
+**	◦ exit
+**	◦ The functions of man 3 stdarg
+**
+**	You have to manage the following conversions: sSpdDioOuUxXcC
+**	• You must manage %%
+**	• You must manage the flags #0-+ and space
+**	• You must manage the minimum field-width
+**	• You must manage the precision
+**	• You must manage the flags hh, h, l, ll, j, et z.
+*/
 
 char *ft_itoa_base(int value, int base, int upperCase)
 {
@@ -206,56 +206,68 @@ void printD(t_env *env, va_list ap)
 
 void printo(t_env *env, va_list ap)
 {
-	env->bytes += env->octothorpe == 1 ? write(1, "0x", 2) : 0;
+	env->bytes += env->octothorpe == 1 ? write(1, "0", 1) : 0;
 	printNums(env, ap, 8, 0);
 }
 
 void printO(t_env *env, va_list ap)
 {
-	env->bytes += env->octothorpe == 1 ? write(1, "0X", 2) : 0;
+	env->bytes += env->octothorpe == 1 ? write(1, "0", 1) : 0;
 	printUNums(env, ap, 8, 0);
+}
+
+void printx(t_env *env, va_list ap)
+{
+	env->bytes += env->octothorpe == 1 ? write(1, "0x", 2) : 0;
+	printNums(env, ap, 16, 0);
+}
+
+void printX(t_env *env, va_list ap)
+{
+	env->bytes += env->octothorpe == 1 ? write(1, "0X", 2) : 0;
+	printNums(env, ap, 16, 0);
 }
 
 
 void parseFlag(t_env *env, const char * restrict format)
 {
-	char flagFound;
+    char flagFound;
 
-	flagFound = 1;
-	while (flagFound)
-	{
-		flagFound = 1;
-		if (format[(env->curr)] == '#')
-		{
-			env->octothorpe = 1;
-			(env->curr)++;
-		}
-		else if (format[(env->curr)] == '0')
-		{
-			env->zero = 1;
-			(env->curr)++;
+    flagFound = 1;
+    while (flagFound)
+    {
+        flagFound = 1;
+        if (format[(env->curr)] == '#')
+        {
+            env->octothorpe = 1;
+            (env->curr)++;
+        }
+        else if (format[(env->curr)] == '0')
+        {
+            env->zero = 1;
+            (env->curr)++;
 
-		}
-		else if (format[(env->curr)] == '-')
-		{
-			env->minus = 1;
-			(env->curr)++;
+        }
+        else if (format[(env->curr)] == '-')
+        {
+            env->minus = 1;
+            (env->curr)++;
 
-		}
-		else if (format[(env->curr)] == '+')
-		{
-			env->plus = 1;
-			(env->curr)++;
+        }
+        else if (format[(env->curr)] == '+')
+        {
+            env->plus = 1;
+            (env->curr)++;
 
-		}
-		else if (format[(env->curr)] == ' ')
-		{
-			env->space = 1;
-			(env->curr)++;
-		}
-		else
-			flagFound = 0;
-	}
+        }
+        else if (format[(env->curr)] == ' ')
+        {
+            env->space = 1;
+            (env->curr)++;
+        }
+        else
+            flagFound = 0;
+    }
 }
 void parseConversion(t_env *env, const char * restrict format, va_list ap)
 {
@@ -272,7 +284,7 @@ void parseConversion(t_env *env, const char * restrict format, va_list ap)
 		printd(env, ap);
 	else if (format[(env->curr)] == 'D')
 		printD(env, ap);
-	// else if (format[(env->curr)] == 'i')  implemented above with 'd'
+// else if (format[(env->curr)] == 'i')  implemented above with 'd'
 	else if (format[(env->curr)] == 'o')
 		printo(env, ap);
 	else if (format[(env->curr)] == 'O')
@@ -281,9 +293,9 @@ void parseConversion(t_env *env, const char * restrict format, va_list ap)
 		printUNums(env, ap, 10, 0);
 	// else if (format[(env->curr)] == 'U')
 	else if (format[(env->curr)] == 'x')
-		printNums(env, ap, 16, 0);
+		printx(env, ap);
 	else if (format[(env->curr)] == 'X')
-		printNums(env, ap, 16, 1);
+		printX(env, ap);
 	else if (format[(env->curr)] == 'c')
 		printc(env, ap);
 	else if (format[(env->curr)] == 'C')
@@ -299,29 +311,291 @@ int ft_printf(const char * restrict format, ...)
 
 	env.curr		= 0;
 	env.bytes		= 0;
-	reset_env(&env);
+  	reset_env(&env);
 	while (format[(env.curr)])
 	{
-		if (format[(env.curr)] == '%')
-		{
-			if(format[(env.curr + 1)] == ' ')
-			{
-				while (format[(env.curr + 1)] == ' ')
-					(env.curr)++;
-				write(1, " ", 1);
-				(env.bytes)++;
+		 if (format[(env.curr)] == '%')
+		 {
+			 if(format[(env.curr + 1)] == ' ')
+			 {
+				 while (format[(env.curr + 1)] == ' ')
+					 (env.curr)++;
+				 write(1, " ", 1);
+				 (env.bytes)++;
 
-			}
-			parseConversion(&env, format, ap);
-		}
+			 }
+			 parseConversion(&env, format, ap);
+		 }
 		else
 		{
 			write(1, &(format[(env.curr)]), 1);
 			(env.bytes)++;
 			(env.curr)++;
 		}
-
+		
 	}
 	return (env.bytes);
 }
+/*
+int main(void)
+{
+	int ret;
 
+	ft_printf("\n");
+	ft_printf(" %%%% test\n");
+	ret = ft_printf("printing |%%|");
+	ft_printf("\n");
+	printf("ret is=|%i|\n", ret);
+
+	ft_printf("\n");
+	ft_printf("\n");
+
+	ft_printf("Char test\n");
+	ft_printf("\n");
+	char c;
+
+	c = 'x';
+
+	char d;
+
+	d = 'y';
+
+	ret = ft_printf("printing |%c| |%c|", c, d);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+
+
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("String test\n");
+	ft_printf("\n");
+	char *strng = "Hello";
+
+	ret = ft_printf("printing |%s|", strng);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("d test\n");
+	ft_printf("\n");
+	int dtest = 1234567890;
+
+	ret = ft_printf("printing |%d|", dtest);	
+	ft_printf("\n");
+	
+	ft_printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("x test\n");
+	ft_printf("\n");
+	int xtest = 1234567890;
+
+	ret = ft_printf("printing |%x|", xtest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+		////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("X test\n");
+	ft_printf("\n");
+	int Xtest = 1234567890;
+
+	ret = ft_printf("printing |%X|", Xtest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("o test\n");
+	ft_printf("\n");
+	int otest = 1234567890;
+
+	ret = ft_printf("printing |%o|", otest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("O test\n");
+	ft_printf("\n");
+	int Otest = 1234567890;
+
+	ret = ft_printf("printing |%O|", Otest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("u test\n");
+	ft_printf("\n");
+	unsigned int utest = 1234567890;
+
+	ret = ft_printf("printing |%u|", utest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("p test\n");
+	ft_printf("\n");
+	unsigned int ptest = 1234567890;
+
+	ret = ft_printf("printing |%p|", &ptest);	
+	printf("\nprintfs: |%p|\n", &ptest);
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("p test\n");
+	ft_printf("\n");
+	wchar_t wtest = L'Ω';
+	setlocale(LC_CTYPE, "");
+
+	ret = ft_printf("printing |%C|", wtest);	
+	printf("\nprintfs: |%C|\n", wtest);
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("o test with #\n");
+	ft_printf("\n");
+	int oOcttest = 1234567890;
+
+	ret = ft_printf("printing |%#o|", oOcttest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("O test with #\n");
+	ft_printf("\n");
+	int OOcttest = 1234567890;
+
+	ret = ft_printf("printing |%#O|", OOcttest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("x test with #\n");
+	ft_printf("\n");
+	int oxtest = 1234567890;
+
+	ret = ft_printf("printing |%#x|", oxtest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+	////////////////////////
+	ft_printf("\n");
+	ft_printf("\n");
+
+
+	ft_printf("X test with #\n");
+	ft_printf("\n");
+	int oXtest = 1234567890;
+
+	ret = ft_printf("printing |%#X|", oXtest);	
+	ft_printf("\n");
+	
+	printf("ret is=|%i|\n", ret);
+	////////////////////////
+
+
+
+	printf("\n\nnormal printf: printing |%#x|, |%#X|\n\n", oxtest, oXtest);	
+
+	printf("\n\nnormal printf: printing |%#o|, |%#O|\n\n", oOcttest, OOcttest);	
+
+	printf("\n\n\n\nAssorted Printf experiments below\n");
+
+	int test = 100;
+	printf("\n100 with %%o =|%o|", test);
+	printf("\nusing ft_itoa_base: %s", ft_itoa_base(test, 8, 0));
+	printf("\nBelow will \"print Hello right justified in a group of 25 spaces.\"");
+	printf("\n|%25.4s|","Hello");
+	printf("\n%lu", strlen("Hello"));
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	return (0);
+}
+
+*/
