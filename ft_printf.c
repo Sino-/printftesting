1459f23657c0abbb6615a0f57dab14d00a961cbc
Sino- <SinisterSino@gmail.com>
2017-07-28 00:14:33 -0700

    testing padding

--- ft_printf.c
+++ ft_printf.c
@@ -211,11 +211,11 @@ void	print_padding(t_env *env, char *str, char padding)
 {
 	if (env->width)
 	{
-		while (((env->width) - ft_strlen(str)) > 0)
+		while (((env->width)-- - ft_strlen(str)) > 0)
 		{
 			write(1, &padding, 1);
 			(env->bytes)++;
-			(env->width)--;
+			//(env->width)--;
 		}
 	}
 }
