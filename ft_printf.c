3e04582c9fbe95668b72650caa4542e9255659e3
Sino- <SinisterSino@gmail.com>
2017-08-08 17:56:19 -0700

    testing precision

--- ft_printf.c
+++ ft_printf.c
@@ -217,7 +217,7 @@ void	print_padding(t_env *env, char *str, char padding)
 			write(1, &padding, 1);
 			(env->bytes)++;
 			(env->width)--;
-			printf("Printed = |%d|", printed);
+			printf("\n Printed = |%d|\n", printed++;);
 		}
 		 printf("\n\n WIDTH IS |%d|", (env->width));
 		// while ((env->precision) > 0)
