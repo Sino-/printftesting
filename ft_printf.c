4e59ddc96eea491aa81b154490ada6c5ecf13775
Sino- <SinisterSino@gmail.com>
2017-08-08 17:58:55 -0700

    testing precision

--- ft_printf.c
+++ ft_printf.c
@@ -211,8 +211,8 @@ void	print_padding(t_env *env, char *str, char padding)
 		printf("\n\n WIDTH BEFORE LOOP IS |%d|", (env->width));
 		//int chars_to_print;
 
-		//chars_to_print = (ft_strlen(str) > env->precision) ? ft_strlen(str): env->precision;	
-		while ((env->width) - ft_strlen(str) > 0)
+		chars_to_print = (ft_strlen(str) < env->precision) ? ft_strlen(str): env->precision;	
+		while ((env->width) - chars_to_print > 0)
 		{
 			write(1, &padding, 1);
 			(env->bytes)++;
