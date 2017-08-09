20f40361b59f2f2fa429404eac9c3b5373dbcf35
Sino- <SinisterSino@gmail.com>
2017-08-08 18:29:51 -0700

    testing precision

--- ft_printf.c
+++ ft_printf.c
@@ -217,10 +217,11 @@ void	print_padding(t_env *env, char *str, char padding)
 		int chars_to_print;
 
 		chars_to_print = (ft_strlen(str) < env->precision) ? ft_strlen(str): env->precision;	
-		while ((env->width) - chars_to_print > 0)
+		while ((env->width) + chars_to_print > 0)
 		{
 			write(1, &padding, 1);
 			(env->bytes)++;
+			chars_to_print--;
 			printed++;
 		}
 		// while (printed < (env->precision))
