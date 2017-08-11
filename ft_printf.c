eccfa52a81b99590f56f8aa541008ca51e3ee041
Sino- <SinisterSino@gmail.com>
2017-08-10 19:33:11 -0700

    testing one edge case

--- ft_printf.c
+++ ft_printf.c
@@ -250,25 +250,33 @@ void	print_number_padding(t_env *env, char *str)
 			(env->bytes)++;
 		}
  	}
- // 	else if ((env->zero) && (ft_strlen(str) - env->precision) < env->width)
-	// {
-	// 	while ((env->width)-- > ft_strlen(str))
-	// 	{
-	// 		write(1, " ", 1);
-	// 		(env->bytes)++;
-	// 	}
- // 	}
-	else
+ 	else if ((env->zero) && (ft_strlen(str) - env->precision) < env->width)
 	{
-		//printf("\nWIDTH |%d| || precision |%d| || ft_strlen |%d|\n", (env->width),(env->precision), ft_strlen(str));
+		while ((env->width)-- > ft_strlen(str))
+		{
+			write(1, " ", 1);
+			(env->bytes)++;
+		}
+ 	}
+	else if (env->precision > ft_strlen(str))
+	{
+
 		while ((env->width) > (env->precision))
 		{
-			write(1, "!", 1);
 			write(1, " ", 1);
 			(env->bytes)++;
 			(env->width)--;
 		}
+	{
+	else
+		printf("\nWIDTH |%d| || precision |%d| || ft_strlen |%d|\n", (env->width),(env->precision), ft_strlen(str));
+		while ((env->width)-- > (ft_strlen(str) + (env->precision)))
+		{
+			write(1, " ", 1);
+			(env->bytes)++;
+		}
 	}
+
 }
 
 void	print_number_string(t_env *env, char *str)
