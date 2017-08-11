cd1ebee7e618a77882355e178998b94ca03207b9
Sino- <SinisterSino@gmail.com>
2017-08-10 18:33:39 -0700

    testing number padding function

--- ft_printf.c
+++ ft_printf.c
@@ -250,6 +250,14 @@ void	print_number_padding(t_env *env, char *str)
 			(env->bytes)++;
 		}
  	}
+ 	else if ((env->zero) && (ft_strlen(str) - env->precision) < env->width)
+	{
+		while ((env->width)-- > ft_strlen(str))
+		{
+			write(1, " ", 1);
+			(env->bytes)++;
+		}
+ 	}
 	else
 	{
 		while ((env->width)-- > (ft_strlen(str) + (env->precision)))
