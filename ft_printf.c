eb0f4420ffcb0bd08ef02e20a51a5c5098a4ab0b
Sino- <SinisterSino@gmail.com>
2017-08-10 19:28:05 -0700

    testing one edge case

--- ft_printf.c
+++ ft_printf.c
@@ -260,7 +260,7 @@ void	print_number_padding(t_env *env, char *str)
  	}
 	else
 	{
-		printf("\nWIDTH |%d| || precision |%d| || ft_strlen |%d|\n", (env->width),(env->precision), ft_strlen(str));
+		//printf("\nWIDTH |%d| || precision |%d| || ft_strlen |%d|\n", (env->width),(env->precision), ft_strlen(str));
 		while ((env->width) > (env->precision))
 		{
 			write(1, " ", 1);
