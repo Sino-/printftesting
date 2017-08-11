dc2383c75efdd2ae511953138ff00f36676b5a2e
Sino- <SinisterSino@gmail.com>
2017-08-10 17:03:29 -0700

    testing replacement print padding

--- ft_printf.c
+++ ft_printf.c
@@ -180,27 +180,7 @@ void	printc(t_env *env, va_list ap)
 	(env->bytes)++;
 	(env->curr)++;
 }
-/* TESTING REPLACEMENT BELOW
-void	print_string(t_env *env, char *str)
-{
-	if (env->precision)
-	{
-		while ((env->precision)--)
-		{
-			write(1, str++, 1);
-			(env->bytes)++;
-		}
-	}
-	else
-	{
-		while (*str)
-		{
-			write(1, str++, 1);
-			(env->bytes)++;
-		}
-	}
-}
-*/
+
 void	print_string(t_env *env, char *str)
 {
 	unsigned int	precision;
@@ -225,7 +205,22 @@ void	print_string(t_env *env, char *str)
 	}
 }
 
+void	print_padding(t_env *env, char *str, char padding)
+{
+	int width;
 
+	width = (env->precision > env->width) ? env->precision : env->width;
+	if (width)
+	{
+		while (width - (env->precision) > 0)
+		{
+			write(1, &padding, 1);
+			(env->bytes)++;
+			width--;
+		}
+	}
+}
+/* TESTING REPLACEMENT PRINT PDDING ABOVE
 void	print_padding(t_env *env, char *str, char padding)
 {
 	if (env->width)
@@ -237,7 +232,7 @@ void	print_padding(t_env *env, char *str, char padding)
 		}
 	}
 }
-
+*/
 void	prints(t_env *env, va_list ap)
 {
 	char	*str;
@@ -545,29 +540,6 @@ int		ft_printf(const char *restrict format, ...)
 }
 
 /*
-void	print_string(t_env *env, char *str)
-{
-	unsigned int	precision;
-
-	precision = (env->precision);
-	if (precision)
-	{
-		while (precision)
-		{
-			write(1, str++, 1);
-			(env->bytes)++;
-			precision--;
-		}
-	}
-	else
-	{
-		while (*str)
-		{
-			write(1, str++, 1);
-			(env->bytes)++;
-		}
-	}
-}
 
 void	print_numbers_padding(t_env *env, char *str, char padding)
 {
@@ -584,7 +556,6 @@ void	print_numbers_padding(t_env *env, char *str, char padding)
 		}
 	}
 }
-
 
 void	print_padding(t_env *env, char *str, char padding)
 {
